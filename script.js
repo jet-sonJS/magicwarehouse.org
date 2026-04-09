b928387f1ff25d3ef24f2f642a5ce6266f701b29
ZuhayraM <zunibay58@gmail.com>
2026-04-09 11:33:15 +0100

    Added image zoom to project_info.html and script2.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const productCard = `
-            <div class="product-card">
+const productCard = `<div class="product-card">
                 <img src="product.jpg" alt="Product Image">
                 <h3>Product Name</h3>
                 <small>Product description.</small><br><br>
