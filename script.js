01d7a42ef8600ccbf10885bc035d03881203b422
ZuhayraM <zunibay58@gmail.com>
2026-04-09 09:49:22 +0100

    Added 2 new files and more cool functionality.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const productCard = `
                 <img src="product.jpg" alt="Product Image">
                 <h3>Product Name</h3>
                 <small>Product description.</small><br><br>
-                <button>See more</button>
+                <a href="product_info.html"><button>See more</button></a>
             </div>`;
 
 const section = document.querySelector('section');
