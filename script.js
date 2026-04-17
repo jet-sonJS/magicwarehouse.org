4e49fa73ef0cfc4f696bbe2ed2d395d3eeb61ab8
ZuhayraM <zunibay58@gmail.com>
2026-04-17 10:40:00 +0100

    changed all styling and made responsive

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const productCard = `<div class="product-card">
                 <img src="product_img.jpg" alt="Product Image">
                 <h3>Product Name</h3>
                 <small>Product description.</small><br><br>
-                <a href="product_info.html"><button>See more</button></a>
+                <a href="product_info.html"><button>Details</button></a>
             </div>`;
 
-const section = document.querySelector('section');
+const section = document.querySelector('.product-grid');
 
 let numProducts = 15;
 
@@ -14,8 +14,16 @@ for (let i = 0; i < numProducts; i++) {
 }
 
 function loadMoreProducts() {
-    numProducts += 15;
-    for (let i = 0; i < numProducts; i++) {
+    const newProducts = 15;
+
+    for (let i = 0; i < newProducts; i++) {
         section.innerHTML += productCard;
     }
+
+    numProducts += newProducts;
+}
+
+function toggleMenu() {
+  const nav = document.getElementById("navLinks");
+  nav.classList.toggle("open");
 }
--- script2.js
+++ script2.js
@@ -1,40 +1,41 @@
 function imageZoom(imgID, resultID) {
-  var img, lens, result, cx, cy;
-  img = document.getElementById(imgID);
-  result = document.getElementById(resultID);
-  lens = document.createElement("DIV");
-  lens.setAttribute("class", "img-zoom-lens");
-  img.parentElement.insertBefore(lens, img);
-  cx = result.offsetWidth / lens.offsetWidth;
-  cy = result.offsetHeight / lens.offsetHeight;
-  result.style.backgroundImage = "url('" + img.src + "')";
-  result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
-  lens.addEventListener("mousemove", moveLens);
-  img.addEventListener("mousemove", moveLens);
-  lens.addEventListener("touchmove", moveLens);
-  img.addEventListener("touchmove", moveLens);
-  function moveLens(e) {
-    var pos, x, y;
+  const img = document.getElementById(imgID);
+  const result = document.getElementById(resultID);
+
+  result.style.backgroundImage = `url(${img.src})`;
+
+  function move(e) {
     e.preventDefault();
-    pos = getCursorPos(e);
-    x = pos.x - (lens.offsetWidth / 2);
-    y = pos.y - (lens.offsetHeight / 2);
-    if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
-    if (x < 0) {x = 0;}
-    if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
-    if (y < 0) {y = 0;}
-    lens.style.left = x + "px";
-    lens.style.top = y + "px";
-    result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+
+    const rect = img.getBoundingClientRect();
+
+    const x = (e.touches ? e.touches[0].clientX : e.clientX) - rect.left;
+    const y = (e.touches ? e.touches[0].clientY : e.clientY) - rect.top;
+
+    const xPercent = (x / rect.width) * 100;
+    const yPercent = (y / rect.height) * 100;
+
+    result.style.backgroundPosition = `${xPercent}% ${yPercent}%`;
   }
-  function getCursorPos(e) {
-    var a, x = 0, y = 0;
-    e = e || window.event;
-    a = img.getBoundingClientRect();
-    x = e.pageX - a.left;
-    y = e.pageY - a.top;
-    x = x - window.pageXOffset;
-    y = y - window.pageYOffset;
-    return {x : x, y : y};
+
+  function show() {
+    result.style.display = "block";
   }
+
+  function hide() {
+    result.style.display = "none";
+  }
+
+  img.addEventListener("mouseenter", show);
+  img.addEventListener("mouseleave", hide);
+  img.addEventListener("mousemove", move);
+
+  img.addEventListener("touchstart", show);
+  img.addEventListener("touchmove", move);
+  img.addEventListener("touchend", hide);
+}
+
+function toggleMenu() {
+  const nav = document.getElementById("navLinks");
+  nav.classList.toggle("open");
 }
